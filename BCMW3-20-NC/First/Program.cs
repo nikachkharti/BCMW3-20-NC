e0d4d5ee5ed0dea66079378a8c6cc5399ad7b685
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-09-15 21:36:16 +0400

    Second lecture

--- BCMW3-20-NC/First/Program.cs
+++ BCMW3-20-NC/First/Program.cs
@@ -1,9 +1,13 @@
-﻿namespace First
+﻿using System.Security;
+
+namespace First
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            #region CLASSWORK
+
             // "Hello From C#" -- string
             // 'C' -- char
 
@@ -22,10 +26,6 @@
 
             //byte firstNumber = 12;
             //int secondNumber = Convert.ToInt32(firstNumber);
-
-
-
-
 
 
 
@@ -64,29 +64,186 @@
             //Console.WriteLine($"Hello my name is {firstName} {lastName} I am {age} years old. My personal number is {personalNumber}");
 
 
-            int numberToCheck = 32;
+            //int numberToCheck = 32;
 
-            if (numberToCheck > 50)
-            {
-                Console.WriteLine("MORE");
-            }
-            else if (numberToCheck == 50)
-            {
-                Console.WriteLine("EQUAL");
-            }
-            else if (numberToCheck == 32)
-            {
-                numberToCheck = 13;
-                Console.WriteLine(numberToCheck);
-            }
-            else if (numberToCheck == 51)
-            {
-                Console.WriteLine("51");
-            }
-            else
-            {
-                Console.WriteLine("LESS");
-            }
+            //if (numberToCheck > 50)
+            //{
+            //    Console.WriteLine("MORE");
+            //}
+            //else if (numberToCheck == 50)
+            //{
+            //    Console.WriteLine("EQUAL");
+            //}
+            //else if (numberToCheck == 32)
+            //{
+            //    numberToCheck = 13;
+            //    Console.WriteLine(numberToCheck);
+            //}
+            //else if (numberToCheck == 51)
+            //{
+            //    Console.WriteLine("51");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("LESS");
+            //}
+
+            #endregion
+
+
+            #region HOMEWORK
+
+            // = ერთი ტოლობა ნიშნავს მინიჭებას
+            // == ნიშნავს ორი რაღაცის შედარებას
+
+            //Console.Write("Enter number: ");
+            //int number = int.Parse(Console.ReadLine());
+
+
+            //switch (number)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Number is 1");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Number is 2");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Number is 3");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Other number");
+            //        break;
+            //}
+
+
+            //if (number == 1)
+            //{
+            //    Console.WriteLine("Number is 1");
+            //}
+            //else if (number == 2)
+            //{
+            //    Console.WriteLine("Number is 2");
+            //}
+            //else if (number == 3)
+            //{
+            //    Console.WriteLine("Number is 3");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Other number");
+            //}
+
+
+
+            //Console.Write("Enter age: ");
+            //byte age = byte.Parse(Console.ReadLine());
+
+
+            //ტერნარული ოპერატორი ? :
+            //string isAdult = age >= 18 ? "Adult" : "No adult";
+
+            //if (age >= 18)
+            //{
+            //    isAdult = "Adult";
+            //}
+            //else
+            //{
+            //    isAdult = "No adult";
+            //}
+
+
+
+
+            //const string validAdmin = "admin";
+            //const string validPassword = "123";
+
+
+            //Console.WriteLine("UserName");
+            //string userName = Console.ReadLine();
+
+
+            //Console.WriteLine("Password");
+            //string password = Console.ReadLine();
+
+
+            //if (userName == validAdmin && password == validPassword)
+            //{
+            //    Console.WriteLine("Successful login");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid user");
+            //}
+
+            //string result =
+            //    userName == validAdmin && password == validPassword
+            //    ? "Successful login"
+            //    : "Invalid user";
+
+            //Console.WriteLine(result);
+
+
+
+            //int mark = 87;
+            //switch (mark)
+            //{
+            //    case int m when m >= 90:
+            //        Console.WriteLine("A+");
+            //        break;
+            //    case int m when m >= 80:
+            //        Console.WriteLine("A");
+            //        break;
+            //    case int m when m >= 70:
+            //        Console.WriteLine("B");
+            //        break;
+            //    default:
+            //        Console.WriteLine("C or below");
+            //        break;
+            //}
+
+
+            //decimal amount = 5000;
+            //decimal discount = amount > 1000 ? amount * 0.1m : amount * 0.05m;
+
+            //Console.WriteLine($"Discount price is : {discount} GEL");
+
+
+            //int day = 3;
+
+            //switch (day)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Monday");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Tuesday");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Wednesday");
+            //        break;
+            //    case 4:
+            //        Console.WriteLine("Thursday");
+            //        break;
+            //    case 5:
+            //        Console.WriteLine("Friday");
+            //        break;
+            //    case 6:
+            //        Console.WriteLine("Satuarday");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("Sunday");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid number");
+            //        break;
+            //}
+
+
+
+
+
+            #endregion
 
         }
     }
---
+++ BCMW3-20-NC/Second/Program.cs
@@ -0,0 +1,135 @@
+﻿#region CLASSWORK
+
+
+
+//გამაერთიანებელი
+//string firstName = "Nika";
+//string lastName = "Chkhartishvili";
+
+//string fullName = firstName + " " + lastName;
+
+//Console.WriteLine(fullName);
+
+
+// არითმეტიკული
+//int a = 10;
+//int b = 2;
+
+//Console.WriteLine(a + b);
+//Console.WriteLine(a - b);
+//Console.WriteLine(a * b);
+//Console.WriteLine(a / b); // გაყოფა ---> 5
+//Console.WriteLine(a % b); // ნაშთიანი გაყოფა ---> 0
+
+
+
+// შედარების და მიმართების ოპერატორები
+//int x = 5;
+//int y = 10;
+
+//Console.WriteLine(x == y); // false
+//Console.WriteLine(x != y); // true
+//Console.WriteLine(x > y); // false
+//Console.WriteLine(x < y); // true
+//Console.WriteLine(x >= y); // false
+//Console.WriteLine(x <= y); // false
+
+
+//ლოგიკური ოპერატორები
+//bool a = true;
+//bool b = false;
+
+//Console.WriteLine(a && b); //false (AND)
+//Console.WriteLine(a || b); //true (OR)
+//Console.WriteLine(!a); //false (NOT)
+
+
+//მინიჭების ოპერატორები
+//int n = 5;
+
+////n = n + 2;
+//n += 2;
+
+////n = n - 2;
+//n -= 2;
+
+////n = n * 2;
+//n *= 2;
+
+////n = n / 2;
+//n /= 2;
+
+////n = n % 2;
+//n %= 2;
+
+
+
+//Console.WriteLine(n);
+
+
+//გაზრდა შემცირების ოპერატორები
+//int z = 5;
+
+//Console.WriteLine(z += 1);
+//Console.WriteLine(z++);
+//Console.WriteLine(++z);
+
+//Console.WriteLine(z -= 1);
+//Console.WriteLine(z--);
+//Console.WriteLine(--z);
+
+
+
+
+
+//ტერნარი ოპერატორი
+//int age = 20;
+
+//string result = (age > 18) ? "Adult" : "Not Adult";
+//Console.WriteLine(result);
+
+
+
+
+
+
+//NULL - ზე შემოწმების ოპერატორები
+
+//string name = "Nika";
+
+//string displayName = name ?? "Unknown";
+//Console.WriteLine(displayName); //Nika
+
+
+//string name = null;
+
+//string displayName = name ?? "Unknown";
+//Console.WriteLine(displayName); //Unknown
+
+
+
+
+
+//ტიპური ოპერატორები
+//object obj = "Hello";
+//Console.WriteLine(obj is string); //true
+
+//string castedObject = obj as string;
+//Console.WriteLine(castedObject); // Hello
+
+
+
+
+
+// თუ არ  ვიცით მონაცემი რა ტიპია გამოვიყენოთ GetType()
+//int x = 1;
+//Console.WriteLine(x.GetType());
+
+
+
+
+
+
+#endregion
+
+
