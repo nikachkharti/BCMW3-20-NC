484f0ca4ab3f98422fca1e46bb7661077f3b595c
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-09-29 21:16:09 +0400

    Strings

---
+++ BCMW3-20-NC/Five/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace Five
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string text = "Hello";
+            //string text2 = "HELLO";
+
+            //Console.WriteLine(text.Length); // ტექსტის ზომა
+            //bool result = text.Contains("World"); // შეიცავს თუ არა ტექსტი ქვეტექსტს.
+            //bool result = text.StartsWith("hz", StringComparison.OrdinalIgnoreCase);
+            //bool result = text.EndsWith("hz", StringComparison.OrdinalIgnoreCase);
+
+            //string result = text.ToUpper();
+            //string result = text.ToLower();
+
+            //var result = text.Replace("he", "Zj", StringComparison.OrdinalIgnoreCase);
+            //var result = text.Trim('H', 'e');
+            //var result = text.TrimStart();
+            //var result = text.TrimEnd();
+
+            //string[] splitedText = text.Split(',', StringSplitOptions.TrimEntries);
+            //string joinedText = string.Join('|', splitedText);
+
+            //var result = text.Substring(0, 4);
+
+            //var result = text
+            //    .ToLower()
+            //    .IndexOf("o".ToLower());
+
+            //var result = text
+            //    .ToLower()
+            //    .LastIndexOf("o".ToLower());
+
+
+            //int x = 12;
+            //string xString = x.ToString();
+
+            //var result = text.Equals(text2, StringComparison.OrdinalIgnoreCase);
+
+
+            //var result = text.CompareTo(text2); //0  1  -1
+
+            //char x = 'X';
+            //int test = x;
+
+        }
+
+    }
+}
