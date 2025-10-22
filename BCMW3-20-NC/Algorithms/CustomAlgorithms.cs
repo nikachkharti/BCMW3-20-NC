c4e6a6142892de387524c7a569643e716f51ec4c
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-10-22 20:47:52 +0400

    classwork

--- BCMW3-20-NC/Algorithms/CustomAlgorithms.cs
+++ BCMW3-20-NC/Algorithms/CustomAlgorithms.cs
@@ -1,6 +1,12 @@
 ﻿using Algorithms.Models;
 namespace Algorithms
 {
+    //დელეგატი არის ტიპი რომელსაც შეუძლია მიინიჭოს მეთოდი
+    //დელეგატის სტრუქტურა ზუსტად უნდა ემთხვეოდეს
+    //იმ მეთოდის მისაღებ და დასაბრუნებელ ტიპებს რომელ მეთოდსაც იგი ინიჭებს
+
+    public delegate bool FindDelegate(int input);
+
     public class CustomAlgorithms
     {
         public static Vehicle[] Take(Vehicle[] vehicles, int quantity)
@@ -62,5 +68,49 @@ namespace Algorithms
 
             return vehicles;
         }
+
+        public static int Custom_FirstOrDefault(List<int> listCollection, FindDelegate findDelegate)
+        {
+            for (int i = 0; i < listCollection.Count; i++)
+            {
+                if (findDelegate(listCollection[i]))
+                {
+                    return listCollection[i];
+                }
+            }
+
+            return default;
+        }
+        public static int Custom_FirstOrDefault(int[] arrayCollection, FindDelegate findDelegate)
+        {
+            for (int i = 0; i < arrayCollection.Length; i++)
+            {
+                if (findDelegate(arrayCollection[i]))
+                {
+                    return arrayCollection[i];
+                }
+            }
+
+            return default;
+        }
+        public static int Custom_FirstOrDefault(Dictionary<int, int> dictionaryCollection, FindDelegate findDelegate)
+        {
+            var keys = new List<int>(dictionaryCollection.Keys);
+
+            for (int i = 0; i < keys.Count; i++)
+            {
+                int key = keys[i];
+
+                if (findDelegate(dictionaryCollection[key]))
+                    return dictionaryCollection[key];
+            }
+
+            return default;
+        }
+
+
+        //მინდა რომ დამიწეროთ Custom_IndexOf მეთოდი რომელიც იმუშავებს List - ებისთვის და იმუშავებს
+        //დელეგატის გამოყენებით
+
     }
 }
---
+++ BCMW3-20-NC/Twelve/Program.cs
@@ -0,0 +1,44 @@
+﻿using Algorithms;
+
+namespace Twelve
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrayCollection = [1, 2, 3, 3, 2, 12];
+
+            List<int> listCollection = new();
+            listCollection.Add(1);
+            listCollection.Add(2);
+            listCollection.Add(3);
+            listCollection.Add(3);
+            listCollection.Add(2);
+            listCollection.Add(-12);
+            listCollection.Add(-22);
+
+            var result =
+                CustomAlgorithms
+                .Custom_FirstOrDefault(listCollection, FirstNegativeNumber);
+
+        }
+
+
+        private static bool FirstNegativeNumber(int elementToFind)
+        {
+            return elementToFind < 0;
+        }
+
+        private static bool FirstOddElement(int elementToFind)
+        {
+            return elementToFind % 2 != 0;
+        }
+        private static bool FirstEvenElement(int elementToFind)
+        {
+            return elementToFind % 2 == 0;
+        }
+
+
+
+    }
+}
