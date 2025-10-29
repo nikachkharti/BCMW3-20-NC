1eb7c08276c3408b00734de20a28b0aa63669971
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-10-29 21:48:23 +0400

    Repository empty

---
+++ BCMW3-20-NC/TinyBank.Repository/Implementations/AccountRepository.cs
@@ -0,0 +1,38 @@
+﻿using TinyBank.Repository.Interfaces;
+using TinyBank.Repository.Models;
+
+namespace TinyBank.Repository.Implementations
+{
+    public class AccountRepository : IAccountRepository
+    {
+        public int AddAccount(Account newAccount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteAccount(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Account> GetAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Account> GetAccountsOfCustomer(int customerId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account GetSingleAccount(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateAccount(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ BCMW3-20-NC/TinyBank.Repository/Implementations/CustomerRepository.cs
@@ -0,0 +1,33 @@
+﻿using TinyBank.Repository.Interfaces;
+using TinyBank.Repository.Models;
+
+namespace TinyBank.Repository.Implementations
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        public int AddCustomer(Customer newCustomer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer GetSingleCustomer(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateCustomer(Customer newCustomer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
