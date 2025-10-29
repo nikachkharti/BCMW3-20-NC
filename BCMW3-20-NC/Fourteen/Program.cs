77b47c636eb55f50e472926fe111f39d78586078
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-10-29 20:33:33 +0400

    Json

---
+++ BCMW3-20-NC/Fourteen/Program.cs
@@ -0,0 +1,25 @@
+﻿using Algorithms.Models;
+using System.Text.Json;
+
+namespace Fourteen
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //var path = @"../../../cars.json";
+            //string jsonData = File.ReadAllText(path);
+
+            ////დესერიალიზაცია -> Json - ის ობიექტის C# - ის ობიექტად გადაქცევას
+            ////სერიალიზაცია -> C# - ის ობიექტის, Json ობიექტად გადაქცევას
+
+            //List<Vehicle>? vehicles = JsonSerializer.Deserialize<List<Vehicle>>(jsonData);
+            //string vehiclesAsString = JsonSerializer.Serialize(vehicles, new JsonSerializerOptions { WriteIndented = true });
+
+            //Console.WriteLine(vehiclesAsString);
+
+
+            //File.WriteAllText(@"../../../cars2.json", vehiclesAsString);
+        }
+    }
+}
