5b0e373170870763a1092c250ec884e5292589dc
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-11-17 21:23:17 +0400

    Async await

---
+++ BCMW3-20-NC/TwentyOne.ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace TwentyOne.ConsoleApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string data = await
+                File
+                .ReadAllTextAsync(@"C:\Users\User\Desktop\New folder (2)\BCMW3-20-NC\BCMW3-20-NC\Twenty.ConsoleApp\test.txt");
+
+            //alskndlaknsd --- data
+            //dlasdasdljkasnd  ---- data
+            //alsdnlakjds
+        }
+
+    }
+}
