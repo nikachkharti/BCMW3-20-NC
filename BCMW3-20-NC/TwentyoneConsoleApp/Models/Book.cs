6d668081ff80ce3b6b39c6ce879c91dd570ac357
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-11-19 21:38:52 +0400

    XML

---
+++ BCMW3-20-NC/TwentyoneConsoleApp/Models/Book.cs
@@ -0,0 +1,12 @@
+﻿namespace TwentyoneConsoleApp.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Year { get; set; }
+
+        public override string ToString() => $"[{Id}] [{Title}] by [{Id}] {Author} ({Year})";
+    }
+}
