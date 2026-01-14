9229665267d3b468593c13cd048c7a99327509aa
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-01-14 22:09:55 +0400

    Code

---
+++ BCMW3-20-NC/WebApiFirst/Controllers/StudentsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiFirst.Controllers
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+    }
+
+
+    [ApiController]
+    [Route("[controller]")]
+    public class StudentsController : ControllerBase
+    {
+        private static List<Student> _students = new()
+        {
+            new Student { Id = 1, Name = "Alice" },
+            new Student { Id = 2, Name = "Bob" },
+            new Student { Id = 3, Name = "Charlie" }
+        };
+
+
+        //[HttpGet]
+        //public string GetStudent()
+        //{
+        //    return _students.Select(s => string.Join(", ", s)).ToString();
+        //}
+
+        //[HttpGet]
+        //public string GetStudent([FromQuery] int id)
+        //{
+        //    return _students.FirstOrDefault(x => x.Id == id)?.Name ?? "Not Found";
+        //}
+
+        //[HttpGet("{id}")]
+        //public string GetStudent([FromRoute] int id)
+        //{
+        //    return _students.FirstOrDefault(x => x.Id == id)?.Name ?? "Not Found";
+        //}
+
+
+        [HttpPost]
+        public IActionResult CreateStudent([FromBody] Student student)
+        {
+            _students.Add(student);
+            return NoContent();
+        }
+    }
+}
---
+++ BCMW3-20-NC/WebApiFirst/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace WebApiFirst
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            #region Container
+
+            var builder = WebApplication.CreateBuilder(args); // ქმნის ვებისთვის საჭირო ინფრასტრუქტურას
+
+
+            builder.Services.AddControllers(); // ქმნის კონტროლერების მენეჯმენტ სერვისს
+            builder.Services.AddOpenApi(); // ქმნის OpenAPI სერვისს 
+
+            #endregion
+
+            #region Middleware
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.MapOpenApi();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            app.Run();
+
+            #endregion
+        }
+    }
+}
