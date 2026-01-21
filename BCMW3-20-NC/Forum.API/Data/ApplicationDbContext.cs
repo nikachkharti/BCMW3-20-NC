8434e3f374934e85d4da1f6f0650277bfea1bf77
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-01-21 22:10:33 +0400

    Data Seed [ERROR]

--- BCMW3-20-NC/Forum.API/Data/ApplicationDbContext.cs
+++ BCMW3-20-NC/Forum.API/Data/ApplicationDbContext.cs
@@ -9,6 +9,71 @@ namespace Forum.API.Data
         {
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            Guid topic1Id = Guid.NewGuid();
+            Guid topic2Id = Guid.NewGuid();
+            Guid topic3Id = Guid.NewGuid();
+
+            Guid comment1Id = Guid.NewGuid();
+            Guid comment2Id = Guid.NewGuid();
+
+            modelBuilder.Entity<Topic>().HasData
+            (
+                new Topic()
+                {
+                    Id = topic1Id,
+                    Title = "First Topic",
+                    Content = "Hello World!",
+                    CreateDate = DateTime.Now,
+                    ImageUrl = null,
+                    LastCommentDate = DateTime.Now,
+                    CommentsAreAllowed = true
+                },
+                new Topic()
+                {
+                    Id = topic2Id,
+                    Title = "Second Topic",
+                    Content = "Hello World!",
+                    CreateDate = DateTime.Now,
+                    ImageUrl = null,
+                    LastCommentDate = DateTime.Now,
+                    CommentsAreAllowed = true
+                },
+                new Topic()
+                {
+                    Id = topic3Id,
+                    Title = "Second Topic",
+                    Content = "Hello World!",
+                    CreateDate = DateTime.Now,
+                    ImageUrl = null,
+                    LastCommentDate = DateTime.Now,
+                    CommentsAreAllowed = true
+                }
+            );
+
+            modelBuilder.Entity<Comment>().HasData
+            (
+                new Comment()
+                {
+                    Id = comment1Id,
+                    CommentDate = DateTime.Now.AddDays(1),
+                    Content = "Hello Comment!",
+                    TopicId = topic1Id,
+                },
+                new Comment()
+                {
+                    Id = comment2Id,
+                    CommentDate = DateTime.Now.AddDays(2),
+                    Content = "Hello Comment!",
+                    TopicId = topic2Id,
+                }
+            );
+
+        }
+
         public DbSet<Topic> Topics { get; set; }
         public DbSet<Comment> Comments { get; set; }
     }
---
+++ BCMW3-20-NC/Forum.API/Data/Migrations/20260121180918_DataSeed.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Forum.API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class DataSeed : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Topics",
+                columns: new[] { "Id", "CommentsAreAllowed", "Content", "CreateDate", "ImageUrl", "LastCommentDate", "Title" },
+                values: new object[,]
+                {
+                    { new Guid("1cc11272-2f02-4bca-a0f8-abd32f970a32"), true, "Hello World!", new DateTime(2026, 1, 21, 22, 9, 17, 468, DateTimeKind.Local).AddTicks(9664), null, new DateTime(2026, 1, 21, 22, 9, 17, 468, DateTimeKind.Local).AddTicks(9664), "Second Topic" },
+                    { new Guid("7d5f3b62-71dc-4683-8665-5342b8a17975"), true, "Hello World!", new DateTime(2026, 1, 21, 22, 9, 17, 468, DateTimeKind.Local).AddTicks(8934), null, new DateTime(2026, 1, 21, 22, 9, 17, 468, DateTimeKind.Local).AddTicks(9371), "First Topic" },
+                    { new Guid("e80d9e8d-4fa3-4a55-8bc7-b51fd8542f18"), true, "Hello World!", new DateTime(2026, 1, 21, 22, 9, 17, 468, DateTimeKind.Local).AddTicks(9661), null, new DateTime(2026, 1, 21, 22, 9, 17, 468, DateTimeKind.Local).AddTicks(9662), "Second Topic" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Comments",
+                columns: new[] { "Id", "CommentDate", "Content", "TopicId" },
+                values: new object[,]
+                {
+                    { new Guid("46c36068-13ab-47a3-a1e5-e74e07d888cb"), new DateTime(2026, 1, 23, 22, 9, 17, 469, DateTimeKind.Local).AddTicks(5631), "Hello Comment!", new Guid("e80d9e8d-4fa3-4a55-8bc7-b51fd8542f18") },
+                    { new Guid("dae65b74-fd79-47ab-aa78-df29cae7351b"), new DateTime(2026, 1, 22, 22, 9, 17, 469, DateTimeKind.Local).AddTicks(5052), "Hello Comment!", new Guid("7d5f3b62-71dc-4683-8665-5342b8a17975") }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("46c36068-13ab-47a3-a1e5-e74e07d888cb"));
+
+            migrationBuilder.DeleteData(
+                table: "Comments",
+                keyColumn: "Id",
+                keyValue: new Guid("dae65b74-fd79-47ab-aa78-df29cae7351b"));
+
+            migrationBuilder.DeleteData(
+                table: "Topics",
+                keyColumn: "Id",
+                keyValue: new Guid("1cc11272-2f02-4bca-a0f8-abd32f970a32"));
+
+            migrationBuilder.DeleteData(
+                table: "Topics",
+                keyColumn: "Id",
+                keyValue: new Guid("7d5f3b62-71dc-4683-8665-5342b8a17975"));
+
+            migrationBuilder.DeleteData(
+                table: "Topics",
+                keyColumn: "Id",
+                keyValue: new Guid("e80d9e8d-4fa3-4a55-8bc7-b51fd8542f18"));
+        }
+    }
+}
