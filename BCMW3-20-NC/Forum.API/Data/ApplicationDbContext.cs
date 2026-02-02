beea99d9ffa7a9808850fdddbf262ce5707846df
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2026-02-02 21:26:05 +0400

    Ef core + identity configuration

--- BCMW3-20-NC/Forum.API/Data/ApplicationDbContext.cs
+++ BCMW3-20-NC/Forum.API/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using Forum.API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Forum.API.Data
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions options) : base(options)
         {
@@ -13,71 +15,25 @@ namespace Forum.API.Data
         {
             base.OnModelCreating(modelBuilder);
 
-            // 🔒 Static GUIDs
-            var topic1Id = new Guid("9EC8A152-A054-4DE9-B30A-C9286E03ED72");
-            var topic2Id = new Guid("4B363C0A-6917-46FA-AAC0-815F2FD9351B");
-            var topic3Id = new Guid("F9B01BD8-C9DB-4DA9-A1CD-46606EB0DADF");
+            modelBuilder.Entity<Topic>()
+                .HasOne(t => t.Author)
+                .WithMany(u => u.Topics)
+                .HasForeignKey(t => t.AuthorId)
+                .OnDelete(DeleteBehavior.Restrict);
 
-            var comment1Id = new Guid("A59E8CEF-D838-4E7C-8396-86B1B3A96A98");
-            var comment2Id = new Guid("72775F5B-0878-483E-97EA-2BC255AA8B74");
 
-            // 🔒 Static dates
-            var createDate = new DateTime(2026, 1, 25);
-            var lastCommentDate = new DateTime(2026, 1, 25);
-
-            modelBuilder.Entity<Topic>().HasData(
-                new Topic
-                {
-                    Id = topic1Id,
-                    Title = "First Topic",
-                    Content = "Hello World!",
-                    CreateDate = createDate,
-                    ImageUrl = null,
-                    LastCommentDate = lastCommentDate,
-                    CommentsAreAllowed = true
-                },
-                new Topic
-                {
-                    Id = topic2Id,
-                    Title = "Second Topic",
-                    Content = "Hello World!",
-                    CreateDate = createDate,
-                    ImageUrl = null,
-                    LastCommentDate = lastCommentDate,
-                    CommentsAreAllowed = true
-                },
-                new Topic
-                {
-                    Id = topic3Id,
-                    Title = "Third Topic",
-                    Content = "Hello World!",
-                    CreateDate = createDate,
-                    ImageUrl = null,
-                    LastCommentDate = lastCommentDate,
-                    CommentsAreAllowed = true
-                }
-            );
-
-            modelBuilder.Entity<Comment>().HasData(
-                new Comment
-                {
-                    Id = comment1Id,
-                    CommentDate = new DateTime(2026, 1, 26),
-                    Content = "Hello Comment!",
-                    TopicId = topic1Id
-                },
-                new Comment
-                {
-                    Id = comment2Id,
-                    CommentDate = new DateTime(2026, 1, 27),
-                    Content = "Hello Comment!",
-                    TopicId = topic2Id
-                }
-            );
+            modelBuilder.Entity<ApplicationUser>(entity => entity.ToTable(name: "Users"));
+            modelBuilder.Entity<IdentityRole>(entity => entity.ToTable(name: "Roles"));
+            modelBuilder.Entity<IdentityUserRole<string>>(entity => entity.ToTable(name: "UserRoles"));
+            modelBuilder.Entity<IdentityUserClaim<string>>(entity => entity.ToTable(name: "UserClaims"));
+            modelBuilder.Entity<IdentityUserLogin<string>>(entity => entity.ToTable(name: "UserLogins"));
+            modelBuilder.Entity<IdentityRoleClaim<string>>(entity => entity.ToTable(name: "RoleClaims"));
+            modelBuilder.Entity<IdentityUserToken<string>>(entity => entity.ToTable(name: "UserTokens"));
         }
 
         public DbSet<Topic> Topics { get; set; }
         public DbSet<Comment> Comments { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
     }
 
 }
--- BCMW3-20-NC/Forum.API/Program.cs
+++ BCMW3-20-NC/Forum.API/Program.cs
@@ -1,10 +1,12 @@
 using Forum.API.Data;
+using Forum.API.Entities;
 using Forum.API.Middleware;
 using Forum.API.Repository;
 using Forum.API.Services;
 using Forum.API.Services.Mapping;
 using Mapster;
 using MapsterMapper;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Serilog;
 
@@ -20,7 +22,10 @@ namespace Forum.API
 
             builder.Services.AddControllers();
             builder.Services.AddSwaggerGen();
+
+            //DbContext
             builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("SqlServerLocalConnection")));
+
             builder.Services.AddScoped<ITopicRepository, TopicRepository>();
             builder.Services.AddScoped<ICommentRepository, CommentRepository>();
             builder.Services.AddScoped<ITopicService, TopicService>();
@@ -29,20 +34,36 @@ namespace Forum.API
                 configuration.ReadFrom.Configuration(context.Configuration);
             });
 
-
             //Mapster
             var config = new TypeAdapterConfig();
             MappingConfig.RegisterMappings(config);
             builder.Services.AddSingleton(config);
             builder.Services.AddScoped<IMapper, ServiceMapper>();
 
+            //Identity
+            builder.Services.AddIdentity<ApplicationUser, IdentityRole>(options =>
+            {
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequiredLength = 3;
+
+                options.User.RequireUniqueEmail = true;
+            })
+            .AddEntityFrameworkStores<ApplicationDbContext>()
+            .AddDefaultTokenProviders();
+
+
             var app = builder.Build();
 
             app.UseSwagger();
             app.UseSwaggerUI();
             app.UseMiddleware<ErrorHandlingMiddleware>();
+            app.UseDbAutoUpdate();
             app.UseSerilogRequestLogging();
             app.UseHttpsRedirection();
+            app.UseAuthentication();
             app.UseAuthorization();
             app.MapControllers();
             app.Run();
